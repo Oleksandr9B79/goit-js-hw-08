a25f0ed4cbd4688ab220cc5398516f798cf0d28c
Oleksandr Bazavliak <106233191+Oleksandr9B79@users.noreply.github.com>
2022-10-28 19:33:05 +0300

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+// Add imports above this line
+import { galleryItems } from "./gallery-items";
+// Change code below this line
+
+console.log(galleryItems);
+
+// import SimpleLightbox from "simplelightbox";
+
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+// const galleryInner = document.querySelector(".gallery");
+
+// function createGalleryItem(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<a class="gallery__item" href="${original}">
+//   <img class="gallery__image"
+//    src="${preview}"
+//    alt="${description}" />
+// </a>`;
+//     })
+//     .join("");
+// }
+
+// const galleryMarkUp = createGalleryItem(galleryItems);
+
+// galleryInner.insertAdjacentHTML("beforeend", galleryMarkUp);
+
+// const lightbox = new SimpleLightbox(".gallery a", {
+//   captionsData: "alt",
+//   captionPosition: "bottom",
+//   captionDelay: 250,
+// });
